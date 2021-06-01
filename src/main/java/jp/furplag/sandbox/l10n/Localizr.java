4b103fabb9dcba5e7118b0c5d10c7e76bc302eff
furplag <furplag@furplag.jp>
2021-06-01 11:08:40 +0900

    add Localizr (WiP) .

---
+++ src/main/java/jp/furplag/sandbox/l10n/Localizr.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2018+ furplag (https://github.com/furplag)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.furplag.sandbox.l10n;
+
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * snippet of {@link Locale} and {@link TimeZone} .
+ *
+ * @author furplag
+ *
+ */
+public final class Localizr {
+
+  private Localizr() {; /* Localizer instances should NOT be constructed in standard programming . */}
+
+  /** available Locales . */
+  private static final Map<String, Locale> locales = Collections.unmodifiableMap(Arrays.stream(Locale.getAvailableLocales()).map((l) -> Map.entry(l.toString(), l)).flatMap((e) -> Stream.of(
+      e
+    , Map.entry(e.getKey().toLowerCase(), e.getValue())
+    , Map.entry(Arrays.stream(e.getKey().split("_")).filter(StringUtils::isNotBlank).collect(Collectors.joining("_")), e.getValue())
+    , Map.entry(Arrays.stream(e.getKey().split("_")).filter(StringUtils::isNotBlank).map(String::toLowerCase).collect(Collectors.joining("_")), e.getValue())
+    , Map.entry(Stream.of(e.getValue().getLanguage(), e.getValue().getCountry(), e.getValue().getVariant()).filter(StringUtils::isNotBlank).collect(Collectors.joining("_")), e.getValue())
+    , Map.entry(Stream.of(e.getValue().getLanguage(), e.getValue().getCountry(), e.getValue().getVariant()).filter(StringUtils::isNotBlank).map(String::toLowerCase).collect(Collectors.joining("_")), e.getValue())
+  )).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (prev, next) -> next, HashMap::new)));
+
+  /** available TimeZones . */
+  private static final Map<String, TimeZone> timeZones = Collections.unmodifiableMap(
+    Stream.concat(Arrays.stream(TimeZone.getAvailableIDs()), ZoneId.getAvailableZoneIds().stream())
+      .map((tz) -> Map.entry(tz, TimeZone.getTimeZone(tz)))
+      .flatMap((e) -> Stream.of(e, Map.entry(e.getKey().toLowerCase(), e.getValue())))
+      .filter((e) -> Objects.nonNull(e.getValue()))
+      .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (prev, next) -> next, HashMap::new))
+  );
+
+  /**
+   * simple wrapper for {@link Locale} .
+   *
+   * @param locale locale String, e.g.) en_US, ja_JP_JP
+   * @return {@link Locale}, or {@link Locale#getDefault()} if not match
+   */
+  public static Locale getLocale(String locale) {
+    return locales.getOrDefault(Objects.requireNonNullElse(locale, Locale.getDefault().toString()), Locale.getDefault());
+  }
+
+  /**
+   * simple wrapper for {@link TimeZone} .
+   *
+   * @param timeZone timeZone String, e.g.) UTC, PST, Asia/Tokyo
+   * @return {@link TimeZone}, or {@link TimeZone#getDefault()} if not match
+   */
+  public static TimeZone getTimeZone(String timeZone) {
+    return timeZones.getOrDefault(Objects.requireNonNullElse(timeZone, TimeZone.getDefault().toString()), TimeZone.getDefault());
+  }
+}
---
+++ src/test/java/jp/furplag/sandbox/l10n/LocalizrTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2018+ furplag (https://github.com/furplag)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.furplag.sandbox.l10n;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+public class LocalizrTest {
+
+  @Test
+  void testLocales() {/* @formatter:off */
+    assertFalse(Arrays.stream(Optional.ofNullable(Locale.getAvailableLocales()).orElse(new Locale[] {}))
+      .flatMap((l) -> Stream.of(
+          l.toString()
+        , Arrays.stream(l.toString().split("_")).filter(StringUtils::isNotBlank).collect(Collectors.joining("_"))
+        , Arrays.stream(l.toString().split("_")).filter(StringUtils::isNotBlank).map(String::toLowerCase).collect(Collectors.joining("_"))
+        , Stream.of(l.getLanguage(), l.getCountry(), l.getVariant()).filter(StringUtils::isNotBlank).collect(Collectors.joining("_"))
+        , Stream.of(l.getLanguage(), l.getCountry(), l.getVariant()).filter(StringUtils::isNotBlank).map(String::toLowerCase).collect(Collectors.joining("_"))
+      )).map(Localizr::getLocale).anyMatch(Objects::isNull));
+
+    assertEquals(Locale.getDefault(), Localizr.getLocale(null));
+    assertEquals(Locale.getDefault(), Localizr.getLocale("Not exists"));
+    assertEquals(Locale.getDefault(), Localizr.getLocale(Locale.getDefault().toString()));
+    assertEquals(Locale.getDefault(), Localizr.getLocale(Locale.getDefault().toString().toLowerCase()));
+
+    assertEquals(Locale.ROOT, Localizr.getLocale(""));
+  /* @formatter:on */}
+
+  @Test
+  void testTimeZones() {/* @formatter:off */
+    assertFalse(Stream.concat(Arrays.stream(TimeZone.getAvailableIDs()), ZoneId.getAvailableZoneIds().stream())
+      .flatMap((tz) -> Stream.of(tz, tz.toLowerCase()))
+      .map(Localizr::getTimeZone).anyMatch(Objects::isNull));
+
+    assertEquals(TimeZone.getDefault(), Localizr.getTimeZone(null));
+    assertEquals(TimeZone.getDefault(), Localizr.getTimeZone(""));
+    assertEquals(TimeZone.getDefault(), Localizr.getTimeZone("Not exists"));
+    assertEquals(TimeZone.getDefault(), Localizr.getTimeZone(ZoneId.systemDefault().toString()));
+    assertEquals(TimeZone.getDefault(), Localizr.getTimeZone(TimeZone.getDefault().toString()));
+    assertEquals(TimeZone.getDefault(), Localizr.getTimeZone(ZoneId.systemDefault().toString().toLowerCase()));
+    assertEquals(TimeZone.getDefault(), Localizr.getTimeZone(TimeZone.getDefault().toString().toLowerCase()));
+  /* @formatter:on */}
+}
